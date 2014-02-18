d67d09e37aa70b46d25bb552bae6051536aa2632
David Ding <chengjie.ding@gmail.com>
2014-02-18 23:39:06 +0800

    re-format output of the fahrenheit-celsius table

--- chapter01/fctable.c
+++ chapter01/fctable.c
@@ -7,8 +7,8 @@
  */
 int main( void )
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;
   upper = 300;
@@ -16,8 +16,8 @@ int main( void )
 
   fahr = lower;
   while( fahr <= upper ) {
-    celsius = 5 * ( fahr - 32 ) / 9;
-    printf( "%d\t%d\n", fahr, celsius );
+    celsius = ( 5.0 / 9.0 ) * ( fahr - 32.0 );
+    printf( "%3.0f %6.1f\n", fahr, celsius );
     fahr = fahr + step;
   }
 
